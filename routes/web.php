b85b4812002e9b4e69f78310795f45e9e4e8aa0e
nalofree <nalofree@gmail.com>
2018-03-29 02:36:54 +0800

    hercab

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('register/confirm/{token}', 'Auth\RegisterController@confirmEmail');
 
-Route::get('/profile', 'UserController@edit');
-Route::get('/company', 'CompanyController@edit');
-Route::get('/projects', 'ProjectController@edit');
+Route::get('profile', 'UserController@edit');
+Route::get('company', 'CompanyController@edit');
+Route::get('projects', 'ProjectController@edit');

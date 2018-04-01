3a7cd9eb154c504ca71b66a3bec15b045ccb1f4e
nalofree <nalofree@gmail.com>
2018-04-02 05:21:12 +0800

    hercab

--- database/migrations/2018_04_01_211915_add_company_field_to_users_table.php
+++ database/migrations/2018_04_01_211915_add_company_field_to_users_table.php
@@ -14,7 +14,7 @@ class AddCompanyFieldToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->штеупук('company_id');
+            $table->integer('company_id');
         });
     }
 
